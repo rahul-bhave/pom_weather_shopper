00b8ff19c92a8cb7f015ae6658638669f7d1b8eb
Rahul.Bhave <rahul.bhave@qxf2.com>
2019-10-29 17:58:03 +0530

    locators

--- conf/locators_conf.py
+++ conf/locators_conf.py
@@ -12,10 +12,29 @@
 #XPATH
 ###########################################
 
-#Main page
+
 temp_field = "xpath,//span[@id ='temperature']"
 click_moisturizers = "xpath,//button[text()='Buy moisturizers']"
 click_sunscreens= "xpath,//button[text()='Buy sunscreens']"
+heading_moisturizer = "xpath,//h2[contains(text(),'Moisturizers')]"
+heading_sunscreen = "xpath,//h2[contains(text(),'Sunscreens')]"
+page_title = "xpath,//h2"
+moisturizers_names = "xpath,//p[contains(@class,'font-weight-bold top-space-10')]"
+sunscreens_names ="xpath,//p[contains(@class,'font-weight-bold top-space-10')]"
+moisturizers_price = "xpath,//p[contains(text(),'Price')]"
+sunscreens_price = "xpath,//p[contains(text(),'Price')]"
+product_price_element = "xpath,//p[contains(text(), '%s')]/following-sibling::p"
+product_add_element ="xpath,//p[contains(text(), '%s')]/following-sibling::p[contains(text(),'%s')]/following-sibling::button"
+click_cart = "xpath,//button[contains(@class,'nav-link')]"
+checkout_heading = "xpath,//h2"
+pay_with_card = "xpath,//button['Pay with Card']"
+iframe_name = "//iframe[@name='stripe_checkout_app']"
+email = "xpath,//input[@type='email']"       
+card_number  =  "xpath,//input[@type='tel']"
+card_expiry = "xpath,//input[@placeholder='MM / YY']"
+cvc = "xpath,//input[@placeholder='CVC']"
+zip_code = "xpath, //input[@placeholder='ZIP Code']"
+pay_button = "xpath, //button[@type ='submit']"
 
 
 
